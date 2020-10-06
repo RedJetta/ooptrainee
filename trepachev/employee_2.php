46b295ee4a5d35e9f4a747de3e32cc72fa59c719
Maks <mms98@inbox.ru>
2020-10-06 12:18:12 +0300

    Getters and Setters

--- trepachev/employee_2.php
+++ trepachev/employee_2.php
@@ -14,13 +14,52 @@ class Employee{
 
     public function getSalary()
     {
-        return $this->salary;
+        return $this->salary . '$';
     }
 
+
+    public function setSalary($salary)
+    {
+        $this->salary = $salary;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+
+    public function setAge($age)
+    {
+        if ($this->isAgeCorrect($age)) {
+            $this->age = $age;
+        }else return false;
+    }
+
+    private function isAgeCorrect($age){
+        if ($age >= 1 && $age <= 100){
+            return true;
+        }else return false;
+    }
 
 }
 
 $emp1 = new Employee('Vasya', 25, 1250);
 $emp2 = new Employee('Petr', 30, 1700);
-echo $emp1->getSalary() + $emp2->getSalary();
+echo $emp1->getSalary() . ' | ' .  $emp2->getSalary() . '<hr>';
+$emp2->setAge(150);
+echo $emp2->getAge();
 ?>
